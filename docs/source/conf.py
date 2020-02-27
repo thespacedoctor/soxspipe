aa1157e4879e316eb1c1ac1d49b472a8070293d4
Dave <d.r.young@qub.ac.uk>
2020-02-27 17:32:21 +0000

    x

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -4,7 +4,6 @@ from builtins import str
 import sys
 import os
 from datetime import datetime, date, time
-from soxspipe import cl_utils
 from mock import Mock as MagicMock
 import re
 from sphinx_markdown_parser.parser import MarkdownParser, CommonMarkParser
@@ -24,7 +23,7 @@ class Mock(MagicMock):
 MOCK_MODULES = ['numpy', 'scipy', 'matplotlib', 'matplotlib.colors',
                 'matplotlib.pyplot', 'matplotlib.cm', 'matplotlib.path', 'matplotlib.patches', 'matplotlib.projections', 'matplotlib.projections.geo', 'healpy', 'astropy', 'astropy.io', 'pylibmc', 'HMpTy', 'HMpTy.mysql', 'ligo', 'ligo.gracedb', 'ligo.gracedb.rest', 'astropy.nddata', 'astropy.units.quantity', 'astropy.modeling', 'astropy.wcs.utils', 'astropy.utils', 'ccdproc', 'astropy.stats', 'reproject', 'reproject.spherical_intersect', 'reproject.spherical_intersect._overlap']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
-
+from soxspipe import cl_utils
 
 # WHERE DOES THIS conf.py FILE LIVE?
 moduleDirectory = os.path.dirname(os.path.realpath(__file__))
