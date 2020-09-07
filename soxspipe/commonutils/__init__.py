699205520512f9d572c4e8797792e3a5cfdd8f29
Dave <d.r.young@qub.ac.uk>
2020-09-07 20:16:47 +0100

    x

--- soxspipe/commonutils/__init__.py
+++ soxspipe/commonutils/__init__.py
@@ -1,7 +1,9 @@
 """
 *common tools used throughout package*
 """
-from .set_of_files import set_of_files
+from . import set_of_files
 from .keyword_lookup import keyword_lookup
 from .detector_lookup import detector_lookup
 from .create_dispersion_map import create_dispersion_map
+# __all__ = ['set_of_files', 'keyword_lookup',
+#            'detector_lookup', 'create_dispersion_map']
