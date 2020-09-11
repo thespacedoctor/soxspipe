92df6a94ded13ee57f111c5809d9c6f053c6114c
Dave <d.r.young@qub.ac.uk>
2020-09-11 10:04:20 +0100

    pixel arrays now generated indetect continuum

--- soxspipe/commonutils/detect_continuum.py
+++ soxspipe/commonutils/detect_continuum.py
@@ -19,6 +19,13 @@ from soxspipe.commonutils import keyword_lookup
 from soxspipe.commonutils import detector_lookup
 from os.path import expanduser
 import numpy as np
+import unicodecsv as csv
+from soxspipe.commonutils.polynomials import chebyshev_order_wavelength_polynomials
+import matplotlib.pyplot as plt
+from astropy.stats import mad_std
+from astropy.modeling import models, fitting
+from scipy.signal import find_peaks
+from random import random
 
 
 class detect_continuum(object):
@@ -110,10 +117,26 @@ class detect_continuum(object):
         self.log.debug('starting the ``get`` method')
 
         orderNums, waveLengthMin, waveLengthMax = self.read_spectral_format()
-        self.create_pixel_arrays(
+        pixelArrays = self.create_pixel_arrays(
             orderNums,
             waveLengthMin,
             waveLengthMax)
+
+        sliceLength = self.settings[
+            "soxs-order-centre"]["slice-length"]
+        peakSigmaLimit = self.settings[
+            "soxs-order-centre"]["peak-sigma-limit"]
+
+        for order, xy_array in pixelArrays.items():
+            found_xy_array = []
+            for xy in xy_array:
+                found_xy = self.fit_1d_gaussian_to_slice(
+                    xy, sliceLength, peakSigmaLimit)
+                if found_xy:
+                    found_xy_array.append(found_xy)
+
+            percent = 100 * len(found_xy_array) / len(xy_array)
+            print(f"{len(found_xy_array)} out of {len(xy_array)} found ({percent:3.0f}%)")
 
         detect_continuum = None
 
@@ -167,37 +190,131 @@ class detect_continuum(object):
             - ``waveLengthMax`` -- a list of the minimum wavelengths reached by each order
 
         **Return:**
-            - None
-
-        **Usage:**
-
-        ```python
-        usage code 
-        ```
-
-        ---
-
-        ```eval_rst
-        .. todo::
-
-            - add usage info
-            - create a sublime snippet for usage
-            - write a command-line tool for this method
-            - update package tutorial with command-line tool info if needed
-        ```
+            - ``pixelArrays`` -- a dictionary of order based pixel arrays
         """
         self.log.debug('starting the ``create_pixel_arrays`` method')
+
+        # READ ORDER SAMPLING RESOLUTION FROM SETTINGS
+        sampleCount = self.settings[
+            "soxs-order-centre"]["order-sample-count"]
+
+        # CREATE THE WAVELENGTH/ORDER ARRAYS TO BE CONVERTED TO PIXELS
+        orderArray = np.asarray([])
+        wavelengthArray = np.asarray([])
+        for o, wmin, wmax in zip(orderNums, waveLengthMin, waveLengthMax):
+
+            wavelengthArray = np.append(wavelengthArray, np.arange(
+                wmin, wmax, (wmax - wmin) / sampleCount))
+            orderArray = np.append(orderArray, np.ones(sampleCount) * o)
+        order_wave = (orderArray, wavelengthArray)
+
+        # SETUP EMPTY PIXEL ARRAYS
+        pixelArrays = {f"o{k:0.0f}": [] for k in orderNums}
 
         # READ THE FILE
         home = expanduser("~")
         dispersion_map = self.dispersion_map.replace("~", home)
-        dispersion_map = np.genfromtxt(
-            dispersion_map, delimiter=',', names=True)
 
-        print(dispersion_map.dtype.names)
+        with open(dispersion_map, 'rb') as csvFile:
+            csvReader = csv.DictReader(
+                csvFile, dialect='excel', delimiter=',', quotechar='"')
+            for row in csvReader:
+                axis = row["axis"]
+                order_deg = int(row["order-deg"])
+                wavelength_deg = int(row["wavelength-deg"])
+                coeff = [float(v) for k, v in row.items() if k not in [
+                    "axis", "order_deg", "wavelength_deg"]]
+                poly = chebyshev_order_wavelength_polynomials(
+                    log=self.log, order_deg=order_deg, wavelength_deg=wavelength_deg).poly
+                if axis == "x":
+                    xcoords = poly(order_wave, *coeff)
+                if axis == "y":
+                    ycoords = poly(order_wave, *coeff)
+        csvFile.close()
+
+        for o, x, y in zip(orderArray, xcoords, ycoords):
+            pixelArrays[f"o{o:0.0f}"].append((x, y))
 
         self.log.debug('completed the ``create_pixel_arrays`` method')
-        return None
+        return pixelArrays
+
+    def fit_1d_gaussian_to_slice(
+            self,
+            xy,
+            sliceLength,
+            peakSigmaLimit):
+        """*cut a slice from the pinhole flat along the cross-dispersion direction centred on pixel position, fit 1D gaussian and return the peak pixel position*
+
+        **Key Arguments:**
+            - ``xy`` -- the x,y pixel coordinate (tuple)
+            - ``sliceLength`` -- length of the slice to cut
+            - ``peakSigmaLimit`` -- number of simga above median. If peak is not above this level slice will be rejected
+
+        **Return:**
+            - ``fit_xy`` -- gaussian fit peak xy position
+        """
+        self.log.debug('starting the ``fit_1d_gaussian_to_slice`` method')
+
+        # CLIP OUT A SLICE TO INSPECT CENTRED AT POSITION
+        halfSlice = sliceLength / 2
+        x_fit = xy[0]
+        y_fit = xy[1]
+        slice = self.pinholeFlat.data[int(y_fit), max(
+            0, int(x_fit - halfSlice)):min(2048, int(x_fit + halfSlice))]
+
+        # CHECK THE SLICE POINTS IF NEEDED
+        if 1 == 0:
+            x = np.arange(0, len(slice))
+            plt.figure(figsize=(8, 5))
+            plt.plot(x, slice, 'ko')
+            plt.xlabel('Position')
+            plt.ylabel('Flux')
+            plt.show()
+
+        # EVALUATING THE MEAN AND STD-DEV FOR PEAK FINDING - REMOVES SLICE
+        # CONTAINING JUST NOISE
+        median_r = np.median(slice)
+        std_r = mad_std(slice)
+        peaks, _ = find_peaks(slice, height=median_r +
+                              peakSigmaLimit * std_r, width=1)
+
+        # CHECK PEAK HAS BEEN FOUND
+        if peaks is None or len(peaks) <= 0:
+            # CHECK THE SLICE POINTS IF NEEDED
+            if 1 == 0:
+                x = np.arange(0, len(slice))
+                plt.figure(figsize=(8, 5))
+                plt.plot(x, slice, 'ko')
+                plt.xlabel('Position')
+                plt.ylabel('Flux')
+                plt.show()
+            return None
+
+        # FIT THE DATA USING A 1D GAUSSIAN - USING astropy.modeling
+        # CENTRE THE GAUSSIAN ON THE PEAK
+        g_init = models.Gaussian1D(
+            amplitude=1000., mean=peaks[0], stddev=1.)
+        # print(f"g_init: {g_init}")
+        fit_g = fitting.LevMarLSQFitter()
+
+        # NOW FIT
+        g = fit_g(g_init, np.arange(0, len(slice)), slice)
+        xpeak = g.mean + max(0, int(x_fit - halfSlice))
+        ypeak = y_fit
+
+        # PRINT A FEW PLOTS IF NEEDED - GUASSIAN FIT OVERLAYED
+        if 1 == 0 and random() < 0.02:
+            x = np.arange(0, len(slice))
+            plt.figure(figsize=(8, 5))
+            plt.plot(x, slice, 'ko')
+            plt.xlabel('Position')
+            plt.ylabel('Flux')
+            guassx = np.arange(0, max(x), 0.05)
+            plt.plot(guassx, g(guassx), label='Gaussian')
+            plt.show()
+
+        self.log.debug('completed the ``fit_1d_gaussian_to_slice`` method')
+        return (xpeak, ypeak)
 
     # use the tab-trigger below for new method
     # xt-class-method
