5c2c91ce15a1b88f614c4573a858a35bed54a20f
Dave <d.r.young@qub.ac.uk>
2023-06-22 16:45:44 +0100

    x

--- soxspipe/commonutils/tests/test_data_organiser.py
+++ soxspipe/commonutils/tests/test_data_organiser.py
@@ -35,11 +35,8 @@ pathToOutputDir = home + "/xshooter-pipeline-data/unittest_data/xsh/data-organis
 
 try:
     shutil.rmtree(pathToOutputDir)
-<<<<<<< Updated upstream
-=======
     import time
     time.sleep(10)
->>>>>>> Stashed changes
 except Exception as e:
     print(e)
     pass
