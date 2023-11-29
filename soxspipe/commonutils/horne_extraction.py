9b78d77640e84aa7e4a256311be35a24402e0b4d
Marco Landoni <marco.landoni@gmail.com>
2023-11-29 17:27:30 +0100

    commit horne fix

--- soxspipe/commonutils/horne_extraction.py
+++ soxspipe/commonutils/horne_extraction.py
@@ -170,24 +170,35 @@ class horne_extraction(object):
         self.twoDMap = fits.open(twoDMapPath)
 
         # MAKE X, Y ARRAYS TO THEN ASSOCIATE WITH WL, SLIT AND ORDER
-        xdim = self.twoDMap[0].data.shape[1]
-        ydim = self.twoDMap[0].data.shape[0]
+        binx = int(self.skySubtractedFrame.header[kw("WIN_BINX")])
+        biny = int(self.skySubtractedFrame.header[kw("WIN_BINY")])
+        xdim = int(self.twoDMap[0].data.shape[1] / binx)
+        ydim = int(self.twoDMap[0].data.shape[0]/ biny)
         xarray = np.tile(np.arange(0, xdim), ydim)
         yarray = np.repeat(np.arange(0, ydim), xdim)
 
         self.skySubtractedFrame.data[self.skySubtractedFrame.data == 0] = np.nan
 
+        if binx > 1 or biny > 1:
+            from astropy.nddata import block_reduce
+            self.twoDMap["WAVELENGTH"].data = block_reduce(self.twoDMap["WAVELENGTH"].data, (biny, binx), func=np.mean)
+            self.twoDMap["SLIT"].data = block_reduce(self.twoDMap["SLIT"].data, (biny, binx), func=np.mean)
+            self.twoDMap["ORDER"].data = block_reduce(self.twoDMap["ORDER"].data, (biny, binx), func=np.mean)
+
+
         try:
+
             self.imageMap = pd.DataFrame.from_dict({
                 "x": xarray,
                 "y": yarray,
-                "wavelength": self.twoDMap["WAVELENGTH"].data.flatten().byteswap().newbyteorder(),
-                "slit_position": self.twoDMap["SLIT"].data.flatten().byteswap().newbyteorder(),
-                "order": self.twoDMap["ORDER"].data.flatten().byteswap().newbyteorder(),
+                "wavelength": self.twoDMap["WAVELENGTH"].data.flatten(),
+                "slit_position": self.twoDMap["SLIT"].data.flatten(),
+                "order": self.twoDMap["ORDER"].data.flatten(),
                 "flux": self.skySubtractedFrame.data.flatten()
             })
             self.imageMap.dropna(how="all", subset=["wavelength", "slit_position", "order"], inplace=True)
         except:
+
             self.imageMap = pd.DataFrame.from_dict({
                 "x": xarray,
                 "y": yarray,
