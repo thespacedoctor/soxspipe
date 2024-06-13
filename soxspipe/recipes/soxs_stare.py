61b3f9c64eba43d4a2597d5913156aaf735f8c4e
Dave <d.r.young@qub.ac.uk>
2024-06-13 11:00:56 +0100

    option to switch of flat detrending in stare mode

--- soxspipe/recipes/soxs_stare.py
+++ soxspipe/recipes/soxs_stare.py
@@ -288,6 +288,8 @@ class soxs_stare(_base_recipe_):
         filterDict = {kw("PRO_CATG"): f"DISP_IMAGE_{arm}"}
         twoDMap = self.inputFrames.filter(**filterDict).files_filtered(include_path=True)[0]
 
+        if not self.recipeSettings["use_flat"]:
+            master_flat = False
         combined_object = self.detrend(
             inputFrame=combined_object, master_bias=master_bias, dark=dark, master_flat=master_flat, order_table=orderTablePath)
 
