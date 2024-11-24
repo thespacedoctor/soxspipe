228a5329aceb4855cfcc5fee48d418a523413a9e
Dave <d.r.young@qub.ac.uk>
2024-11-24 19:46:43 +0000

    x

--- soxspipe/commonutils/detect_continuum.py
+++ soxspipe/commonutils/detect_continuum.py
@@ -633,7 +633,8 @@ class detect_continuum(_base_detect):
                 )
                 mean_res = np.mean(np.abs(orderPixelTable[f'cont_{self.axisA}_fit_res'].values))
 
-                if "order" in self.recipeName.lower() and mean_res > 1:
+                # if "order" in self.recipeName.lower() and mean_res > 1:
+                if mean_res > 1:
                     # BAD FIT ... FORCE A FAIL
                     orderPixelTable = backupOrderPixelTable
                     raise AttributeError("Failed to continuum trace")
